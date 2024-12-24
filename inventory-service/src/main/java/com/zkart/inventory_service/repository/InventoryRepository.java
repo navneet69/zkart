0b23f92a16ae10fde6c80119ffe945c08cc084dd
navneet69 <kumarnavneet25082001@gmail.com>
2024-12-24 18:01:03 +0530

    Implemented api-gateway and configured routes for microservices

--- inventory-service/src/main/java/com/zkart/inventory_service/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/zkart/inventory_service/repository/InventoryRepository.java
@@ -1,7 +1,5 @@
 package com.zkart.inventory_service.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.zkart.inventory_service.model.Inventory;
